9600a2c087c09558f01c79fa6b79c82c5461a7bc
matej-veselovsky <matej.veselovsky97@gmail.com>
2022-07-09 11:54:36 +0200

    Add working dictionary running of Flask

---
+++ dictionary.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+
+def translate(inputWord, direction):
+    with sqlite3.connect("yorani.db") as con:
+        cur = con.cursor()
+
+        if direction == "ytoc":
+            query = "SELECT czech_word FROM czech_words WHERE reference_id IN (SELECT yorani_id FROM yorani_words WHERE yorani_word IS (?) COLLATE NOCASE);"
+        else:
+            query = "SELECT yorani_word FROM yorani_words WHERE yorani_id IN (SELECT reference_id FROM czech_words WHERE czech_word IS (?) COLLATE NOCASE);"
+
+        cur.execute(query, [inputWord])
+        answer = cur.fetchall()
+
+        if answer:
+            return answer
+        else:
+            return 0
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/dictionary", methods=["GET", "POST"])
+def dictionary():
+    outputList = ""
+
+    if request.method == "POST":
+        inputWord = request.form.get("inputWord")
+        direction = request.form.get("direction")
+
+        outputList = translate(inputWord, direction)
+        return render_template("dictionary.html", wordList = outputList)
+
+    return render_template("dictionary.html")
